f3a9d28d2126c02fcf86c4bb7f4d0c13b1a9fc8f
José Henrique Leopoldo e Silva <josehcls@gmail.com>
2019-10-22 17:38:41 -0300

    First Commit

---
+++ BASIC_Compiler/MotorEventos.cs
@@ -0,0 +1,54 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BASIC_Compiler
+{
+    public class MotorEventos
+    {
+        public Evento EventoCorrente { get; set; }
+        public List<Evento> Eventos { get; set; }
+        public List<Evento> EventosPrioritarios { get; set; }
+        public int InstanteExecucao { get; set; }
+        public SeletorRotinas SeletorRotinas { get; set; }
+        public MotorEventos MotorSaida { get; set; }
+        // TODO: Saida mais generica, como uma fila
+        // TODO: Relatorio de execucao
+
+
+        public void Inicializar()
+        {
+
+        }
+
+        public Evento ExtrairProximoEvento()
+        {
+
+        }
+
+        public string Finalizar()
+        {
+
+        }
+
+        public List<Evento> ExecutarEvento(Evento evento)
+        {
+
+        }
+
+        public void ReceberEvento (Evento evento)
+        {
+        
+        }
+
+        public void EnviarEvento (Evento evento)
+        {
+            this.MotorSaida.ReceberEvento(evento);
+        }
+
+        public void Rodar()
+        {
+
+        }
+    }
+}
---
+++ BASIC_Compiler/SeletorRotinas.cs
@@ -0,0 +1,14 @@
+﻿namespace BASIC_Compiler
+{
+    public interface SeletorRotinas
+    {
+
+        // TODO: Eventos de Saida Internos e Externos
+        // TODO: Fila Normal x Prioritaria
+        public List<Evento> ProcessarEvento (Evento evento)
+        {
+
+        }
+
+    }
+}
