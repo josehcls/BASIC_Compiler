32892ca4c950b55a65048a808f8b9a37ffc06461
José Henrique Leopoldo e Silva <josehcls@gmail.com>
2019-11-01 15:38:48 -0300

    Incluindo Automato Finito

---
+++ BASIC_Compiler/Automatos/AutomatoFinito.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BASIC_Compiler.Automatos
+{
+    public class AutomatoFinito
+    {
+        public List<string> Estados { get; set; }
+        public string EstadoInicial { get; set; }
+        public List<Transicao> Transicoes { get; set; }
+        public List<string> EstadosFinais { get; set; }
+
+        public static AutomatoFinito LeituraDispositivo(string path)
+        {
+            //var json = System.IO.File.ReadAllText(path);
+            ////Console.WriteLine(json);
+
+            //AutomatoFinito automato = JsonConvert.DeserializeObject<AutomatoFinito>(json);
+
+            //return automato;
+            throw new NotImplementedException();
+        }
+
+        public void Simulacao(Cabecote cabecote)
+        {
+            if (cabecote.EstadoAtual == null)
+            {
+                PartidaInicial(cabecote);
+                Simulacao(cabecote);
+            }
+            else
+            {
+                string simbolo = cabecote.PosicaoAtual.Value;
+
+                if (simbolo == "#" && ConfereEstadoFinal(cabecote))
+                {
+                    cabecote.Aceito = true;
+                }
+                else
+                {
+                    var estadoAtual = cabecote.EstadoAtual;
+                    var transicoes = BuscaTransicoes(estadoAtual, simbolo);
+                    // Salva posição atual para o caso de mais de uma transição
+                    var posicao = cabecote.PosicaoAtual;
+                    if (!transicoes.Any())
+                    {
+                        if (simbolo == "#")
+                        {
+                            if (ConfereEstadoFinal(cabecote))
+                            {
+                                cabecote.Aceito = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var transicao in transicoes)
+                        {
+                            // Ignora transição caso a entrada já tenha sido aceita
+                            if (!cabecote.Aceito)
+                            {
+                                // Volta posição para o caso de mais de uma transição
+                                cabecote.PosicaoAtual = posicao;
+                                RealizaTransicao(cabecote, transicao);
+                                // Move Cabeçote para a Direita, caso a transição não tenha sido em vazio
+                                if (transicao.Simbolo != " ") cabecote.MoveParaDireita();
+                                Simulacao(cabecote);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void PartidaInicial(Cabecote cabecote)
+        {
+            cabecote.PosicaoAtual = cabecote.Fita.First;
+            cabecote.EstadoAtual = EstadoInicial;
+        }
+
+        List<Transicao> BuscaTransicoes(string estadoAtual, string simbolo)
+        {
+            return Transicoes.Where(t => t.Origem == estadoAtual && (t.Simbolo == simbolo || t.Simbolo == "")).ToList();
+        }
+
+        bool ConfereEstadoFinal(Cabecote cabecote)
+        {
+            return EstadosFinais.Contains(cabecote.EstadoAtual);
+        }
+
+        static void RealizaTransicao(Cabecote cabecote, Transicao transicao)
+        {
+            cabecote.EstadoAtual = transicao.Destino;
+        }
+    }
+    public class Transicao
+    {
+        public virtual string Origem { get; set; }
+        public virtual string Simbolo { get; set; }
+        public virtual string Destino { get; set; }
+    }
+}
+
---
+++ BASIC_Compiler/Automatos/Cabecote.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BASIC_Compiler.Automatos
+{
+    public class Cabecote
+    {
+        public LinkedList<string> Fita { get; set; }
+        public LinkedListNode<string> PosicaoAtual { get; set; }
+        public string EstadoAtual { get; set; }
+        public bool Aceito { get; set; }
+        public bool Erro { get; set; }
+
+        public Cabecote()
+        {
+            Aceito = false;
+            Erro = false;
+        }
+
+        public void MoveParaDireita()
+        {
+            PosicaoAtual = PosicaoAtual.Next;
+        }
+    }
+}
